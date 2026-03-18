be9061016822c66517890e5f1a5f0c6a491dedf8
OmriNR <omrinir12@gmail.com>
2026-03-18 13:36:55 +0200

    Adding users methods and connection to products

---
+++ StoreAPI/Repositories/Interfaces/IUsersRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain;
+
+namespace Repositories.Interfaces;
+
+public interface IUserRepository
+{
+    void CreateCustomer(User user);
+    void UpdateCustomer(User user);
+    void DeleteCustomer(User user);
+    User GetCustomer(string customerId);
+}
