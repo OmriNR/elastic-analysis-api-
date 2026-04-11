a590a37cae284cde3ef586b723e56cd4683a2540
OmriNR <omrinir12@gmail.com>
2026-04-11 20:27:54 +0300

    Adding Domain

---
+++ StoreAPI/Domain/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Domain;
+
+public class User
+{
+    [DataMember(Name = "user_id")]
+    [JsonPropertyName("user_id")]
+    public string UserId { get; set; }
+    
+    [DataMember(Name = "email")]
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+    
+    [DataMember(Name = "password")]
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+}
---
+++ StoreAPI/Domain/UserProperties.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Domain;
+
+public class UserProperties
+{
+    [DataMember(Name = "user_id")]
+    [JsonPropertyName("user_id")]
+    public string UserId { get; set; }
+    
+    [DataMember(Name = "user_name")]
+    [JsonPropertyName("user_name")]
+    public string UserName { get; set; }
+    
+    [DataMember(Name = "age")]
+    [JsonPropertyName("age")]
+    public int Age { get; set; }
+    
+    [DataMember(Name = "gender")]
+    [JsonPropertyName("gender")]
+    public string Gender { get; set; }
+    
+    [DataMember(Name = "location")]
+    [JsonPropertyName("location")]
+    public GeoProperties Location { get; set; }
+    
+    [DataMember(Name = "created_at")]
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
+
+public class GeoProperties
+{
+    [DataMember(Name = "city")]
+    [JsonPropertyName("city")]
+    public string City { get; set; }
+    
+    [DataMember(Name = "country")]
+    [JsonPropertyName("country")]
+    public string Country { get; set; }
+    
+    [DataMember(Name = "address")]
+    [JsonPropertyName("address")]
+    public string Address { get; set; }
+    
+    [DataMember(Name = "zip_code")]
+    [JsonPropertyName("zip_code")]
+    public string ZipCode { get; set; }
+}
