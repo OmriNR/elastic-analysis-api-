57a3edde050e7edbaf21df860911114a9eeb3629
Omri Nir <omrinir12@gmail.com>
2026-04-14 14:34:13 +0300

    Finish images management

--- StoreAPI/Services/Interfaces/IImagesService.cs
+++ StoreAPI/Services/Interfaces/IImagesService.cs
@@ -4,8 +4,8 @@ namespace Services.Interfaces;
 
 public interface IImagesService
 {
-    void UploadProductImage(string productId,  Stream stream, string contentType, out Statuses status, out string error);
-    void UploadUserImage(string userId,  Stream stream, string contentType, out Statuses status, out string error);
+    void UploadProductImage(string productId,  Stream stream, string originalFileName, out Statuses status, out string error);
+    void UploadUserImage(string userId,  Stream stream, string originalFileName, out Statuses status, out string error);
     Stream GetProductImage(string productId, out Statuses status, out string error);
     Stream GetUserImage(string userId, out Statuses status, out string error);
 }
