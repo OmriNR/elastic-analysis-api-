180432cd16a1774dc0c67ab5c5b7736c874d34c5
Omri Nir <omrinir12@gmail.com>
2026-04-28 12:36:10 +0300

    Writing logs to elastic for monitoring

--- OrdersPublisherAPI/Worker/Program.cs
+++ OrdersPublisherAPI/Worker/Program.cs
@@ -1,6 +1,23 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 using Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+var elasticsearchUrl = builder.Configuration["Elasticsearch:Url"] ?? "http://localhost:9200";
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticsearchUrl))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = "orders-worker-logs-{0:yyyy.MM.dd}",
+        NumberOfReplicas = 1,
+        NumberOfShards = 2
+    }).CreateLogger();
+
+builder.Services.AddSerilog();
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
